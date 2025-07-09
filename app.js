76f242695bb94e8f63a442b9648ec77fa92a0905
farhan6389 <farhan84897@gmail.com>
2025-07-09 14:03:37 +0530

    Created a sample birthday app frontend as a part of daily frontend practice

---
+++ app.js
@@ -0,0 +1,63 @@
+let listText = document.querySelector("#listText");
+
+let dpURL = "https://api.thecatapi.com/v1/images/search";
+async function getImage() {
+    const response = await fetch("https://api.thecatapi.com/v1/images/search", {
+        method: "GET",
+        headers: {
+            "x-api-key": "YOUR_REAL_API_KEY",
+            "Content-Type": "application/json"
+        }
+    });
+
+    const data = await response.json();
+    console.log("Cat API response:", data); // Check what’s returned
+    return data[0]?.url; // Optional chaining to avoid undefined errors
+}
+
+
+async function createUserList() {
+
+    let mydata = [
+
+  ["Aisha", "Feb 22"],
+  ["Zayan", "Mar 5"],
+  ["Nora", "Apr 17"],
+  ["Ibrahim", "May 29"],
+  ["Sana", "Jun 14"],
+  ["Omar", "Jul 9"],
+  ["Lina", "Aug 3"],
+  ["Yusuf", "Sep 26"],
+ 
+];
+
+    for (let i = 0; i < mydata.length; i++) {
+        let Uname = document.createElement("p");
+        Uname.classList = "name";
+        Uname.innerHTML = mydata[i][0];
+
+        // Await the actual image URL
+        let imageData = await getImage();
+        let Uimg = document.createElement("img");
+        Uimg.src = imageData;
+        console.log(Uimg.src);
+
+        let Ubday = document.createElement("p");
+        Ubday.classList = "bday";
+        Ubday.innerHTML = mydata[i][1];
+
+        let myDiv = document.createElement("div");
+        myDiv.classList = "data";
+        myDiv.appendChild(Uname);
+        myDiv.appendChild(Ubday);
+
+        let listItem = document.createElement("li");
+        listItem.appendChild(Uimg);
+        listItem.appendChild(myDiv);
+
+        listText.appendChild(listItem);
+    }
+}
+
+
+createUserList();
